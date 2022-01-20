64c73e7201674f47b0d3f84450199163f94c2340
Andre Manchanthasouk <dremanchan@gmail.com>
2022-01-20 10:57:45 -0600

    formatted code.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
-import { createStore, combineReducers, applyMiddleware} from 'redux'
-import logger from 'redux-logger';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App/App";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import logger from "redux-logger";
 
 const pizzaReducer = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_PIZZA_LIST':
-            return [...state, action.payload]
-    }
+  switch (action.type) {
+    case "SET_PIZZA_LIST":
+      return [...state, action.payload];
+  }
 
-    return state;
-
-}
+  return state;
+};
 
 const storeInstance = createStore(
-    combineReducers({
-        pizzaReducer,
-    }),
-    applyMiddleware(logger),
+  combineReducers({
+    pizzaReducer,
+  }),
+  applyMiddleware(logger)
 );
 
 ReactDOM.render(
-<Provider store ={storeInstance}>
+  <Provider store={storeInstance}>
     <App />
-</Provider>, 
-document.getElementById('root'));
+  </Provider>,
+  document.getElementById("root")
+);
