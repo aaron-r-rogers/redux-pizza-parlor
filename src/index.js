0c9902e4fb04417144701c7fc4dc85092f6ab5d6
Andre Manchanthasouk <dremanchan@gmail.com>
2022-01-20 13:25:32 -0600

    sending changes.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./components/App/App";
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { Provider } from 'react-redux';
 
 const pizzaReducer = (state = [], action) => {
   switch (action.type) {
