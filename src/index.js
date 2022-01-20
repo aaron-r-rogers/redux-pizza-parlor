cf6c7f979d5db69e875a1ad714a43a8bf58e21fb
Toivo Hannigan <THannigan046@gmail.com>
2022-01-20 13:20:44 -0600

    added getOrder, useEffect and orderReducer

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+const orderReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_ORDER_LIST':
+        return action.payload
+    }
+    return state;
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const storeInstance = createStore(
+    combineReducers({
+        orderReducer: orderReducer
+    }),
+    applyMiddleware(logger),
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
